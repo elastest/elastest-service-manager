d7bdf333de5735cf1540ab2c87760b25c26d66bc
Andy Edmonds <edmo@zhaw.ch>
2017-11-29 13:49:29 +0100

    more jenkins fun

--- Jenkinsfile
+++ Jenkinsfile
@@ -15,8 +15,6 @@ node('docker'){
                    echo "Error: $e"
                 }
 
-                echo '[INI] connect2ElastestNetwork'
-
                 def containerId= sh (
                     script: 'cat /proc/self/cgroup | grep "docker" | sed s/\\\\//\\\\n/g | tail -1',
                     returnStdout: true
@@ -25,32 +23,44 @@ node('docker'){
                 sh "docker network list"
                 sh "docker network connect elastest_elastest "+ containerId
 
-            	echo '[END] connect2ElastestNetwork'
-
                 try {
                    sh "docker rm -f mongo"
                 } catch(e) {
                    echo "Error: $e"
                 }
 
-                sh "docker run --name mongo -d --rm mongo"
-                // sh "docker inspect mongo"
+                sh "docker run --name mongo -d --rm mongo:latest"
                 sh "docker network connect elastest_elastest mongo"
                 mongoIP = sh (
                     script: 'docker inspect --format=\\"{{.NetworkSettings.Networks.elastest_elastest.IPAddress}}\\" mongo',
                     returnStdout: true
                 ).trim()
                 echo "Mongo container IP=${mongoIP}"
+
+                try {
+                   sh "docker rm -f mysql:latest"
+                } catch(e) {
+                   echo "Error: $e"
+                }
+
+                sh "docker run --name mysql -d --rm mysql:latest"
+                sh "docker network connect elastest_elastest mysql"
+                mysqlIP = sh (
+                    script: 'docker inspect --format=\\"{{.NetworkSettings.Networks.elastest_elastest.IPAddress}}\\" mysql',
+                    returnStdout: true
+                ).trim()
+                echo "MySQL container IP=${mysqlIP}"
             }
 
             stage ("Unit tests"){
                 echo ("Starting unit tests...")
                 echo "Mongo container IP=${mongoIP}"
-                withEnv(["ESM_MONGO_HOST=${mongoIP}"]){
+                withEnv(["ESM_MONGO_HOST=${mongoIP}", "ESM_MYSQL_HOST=${mysqlIP}"]){
                     echo "Mongo container IP=${mongoIP}"
+                    echo "MySQL container IP=${mysqlIP}"
                     sh "ESM_MONGO_HOST=${mongoIP} tox"
                 }
-                // step([$class: 'JUnitResultArchiver', testResults: '**/nosetests.xml'])
+                step([$class: 'JUnitResultArchiver', testResults: '**/nosetests.xml'])
             }
 
             stage "Build image - Package"
--- tests/test_sql_db.py
+++ tests/test_sql_db.py
@@ -1,46 +1,46 @@
-# # Copyright © 2017-2019 Zuercher Hochschule fuer Angewandte Wissenschaften.
-# # All Rights Reserved.
-# #
-# #    Licensed under the Apache License, Version 2.0 (the "License"); you may
-# #    not use this file except in compliance with the License. You may obtain
-# #    a copy of the License at
-# #
-# #         http://www.apache.org/licenses/LICENSE-2.0
-# #
-# #    Unless required by applicable law or agreed to in writing, software
-# #    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# #    License for the specific language governing permissions and limitations
-# #    under the License.
+# Copyright © 2017-2019 Zuercher Hochschule fuer Angewandte Wissenschaften.
+# All Rights Reserved.
 #
-# # GENERICS
-# from adapters.store import SQLStore
-# from unittest.mock import patch
-# # TESTS
-# from unittest import skipIf
-# import unittest
-# import os
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
 #
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
-# # TODO: move to test_store_backends module
-#
-# if os.getenv('MYSQL_TESTS', 'NO') == 'YES':
-#     SQLStore.set_up()
-#
-#
-# @skipIf(os.getenv('MYSQL_TESTS', 'NO') != 'YES', "MYSQL_TESTS not set in environment variables")
-# class TestCaseManifestManifest(unittest.TestCase):
-#
-#     def test_db_connect_successful(self):
-#         connection = SQLStore.get_connection()
-#         self.assertIsNotNone(connection)
-#         connection.close()
-#         wait_time = 0
-#         SQLStore.set_up(wait_time)
-#
-#     @patch.object(SQLStore, 'get_connection')
-#     def test_db_connect_not_successful(self, mock_connection):
-#         wait_time = 0
-#         mock_connection.return_value = None
-#         with self.assertRaises(Exception):
-#             SQLStore.set_up(wait_time)
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# GENERICS
+from adapters.store import SQLStore
+from unittest.mock import patch
+# TESTS
+from unittest import skipIf
+import unittest
+import os
+
+
+# TODO: move to test_store_backends module
+
+if os.getenv('MYSQL_TESTS', 'NO') == 'YES':
+    SQLStore.set_up()
+
+
+@skipIf(os.getenv('MYSQL_TESTS', 'NO') != 'YES', "MYSQL_TESTS not set in environment variables")
+class TestCaseManifestManifest(unittest.TestCase):
+
+    def test_db_connect_successful(self):
+        connection = SQLStore.get_connection()
+        self.assertIsNotNone(connection)
+        connection.close()
+        wait_time = 0
+        SQLStore.set_up(wait_time)
+
+    @patch.object(SQLStore, 'get_connection')
+    def test_db_connect_not_successful(self, mock_connection):
+        wait_time = 0
+        mock_connection.return_value = None
+        with self.assertRaises(Exception):
+            SQLStore.set_up(wait_time)
