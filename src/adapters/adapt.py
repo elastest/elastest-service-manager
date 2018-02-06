7695cde07b2eb4065b2612b4d9b9b3f9e70e9e28
Andy Edmonds <edmo@zhaw.ch>
2018-02-06 14:04:57 +0100

    minor code refactor

--- src/adapters/adapt.py
+++
@@ -1,8 +0,0 @@
-class Measurer:
-    """Purpose is to measure service level metrics, not considered by EMP"""
-    pass
-
-
-class Remediator:
-    """Purpose is to adjust instance based on service and resource level metrics"""
-    pass
