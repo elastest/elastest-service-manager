6ecbb46da47546dd9a6470a4d188db261e0e2d00
Andy Edmonds <edmo@zhaw.ch>
2018-06-19 11:26:27 +0200

    temp. disables mysql driver, initial fix for mysql

--- src/adapters/store.py
+++ src/adapters/store.py
@@ -726,18 +726,18 @@ class InMemoryStore(Store):  # pragma: no cover
         return True
 
 
-LOG.warn('selecting the host...')
+LOG.warning('selecting the host...')
 mongo_host = os.environ.get('ESM_MONGO_HOST', '')
-sql_host = os.environ.get('ESM_SQL_HOST', os.environ.get('ET_EDM_MYSQL_HOST', ''))
+# sql_host = os.environ.get('ESM_SQL_HOST', os.environ.get('ET_EDM_MYSQL_HOST', ''))
 
-if len(mongo_host) and len(sql_host):  # pragma: no cover
-    raise RuntimeError('Both MongoDB and SQL datastore environment variables are set. Set and use only one.')
+# if len(mongo_host) and len(sql_host):  # pragma: no cover
+#     raise RuntimeError('Both MongoDB and SQL datastore environment variables are set. Set and use only one.')
 
 if len(mongo_host):  # pragma: no cover
     STORE = MongoDBStore(mongo_host)
-elif len(sql_host):  # pragma: no cover
-    LOG.warn('Initial SQLStore host setup...')
-    STORE = SQLStore()
-    STORE.set_up()
+# elif len(sql_host):  # pragma: no cover
+#     LOG.warn('Initial SQLStore host setup...')
+#     STORE = SQLStore()
+#     STORE.set_up()
 else:  # pragma: no cover
     STORE = InMemoryStore()
--- tests/manifests/get_et_manifests.py
+++ tests/manifests/get_et_manifests.py
@@ -1,3 +1,4 @@
+import json
 import requests
 import yaml
 
@@ -9,11 +10,50 @@ services = [
     'elastest-security-service'
 ]
 
+register_service = True
+headers = {
+            'Accept': "application/json",
+            'Content-Type': "application/json",
+            'X-Broker-Api-Version': "2.12",
+        }
+
 for svc in services:
     print("Getting the manifest of the {svc} service.".format(svc=svc))
     res = requests.get('https://raw.githubusercontent.com/elastest/{svc}/master/elastestservice.json'.format(svc=svc))
     content = res.json()
+
+    if register_service:
+        # register service description
+        url = "http://localhost:8080/v2/et/catalog"
+        payload = json.dumps(content['register'])
+        response = requests.request("PUT", url, data=payload, headers=headers)
+
+        # register manifest description
+        url = "http://localhost:8080/v2/et/manifest/{id}".format(id=content['manifest']['id'])
+        payload = json.dumps(content['manifest'])
+        response = requests.request("PUT", url, data=payload, headers=headers)
+
+    f = open('./elastestservice-{svc}.json'.format(svc=content['register']['short_name'].lower()), 'w')
+    f.write(json.dumps(content))
+    f.close()
+
     dc = yaml.load(content['manifest']['manifest_content'])
     f = open('./docker-compose-{svc}.yml'.format(svc=content['register']['short_name'].lower()), 'w')
     f.write(yaml.dump(dc))
     f.close()
+
+# validate number of services registered
+url = "http://localhost:8080/v2/catalog"
+response = requests.request("GET", url, headers=headers)
+print("Should be {expected} service registered. There are: {actual}"
+      .format(expected=len(services), actual=len(response.json()['services'])))
+if len(services) == len(response.json()['services']):
+    print("ALL SERVICES OK!")
+
+url = "http://localhost:8080/v2/et/manifest"
+response = requests.request("GET", url, headers=headers)
+
+print("Should be {expected} manifests registered. There are: {actual}"
+      .format(expected=len(services), actual=len(response.json())))
+if len(services) == len(response.json()):
+    print("ALL MANIFESTS OK!")
