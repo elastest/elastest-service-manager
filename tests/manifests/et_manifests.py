5d09a1d89486ab0a78ba5aae7bb95a3151d41213
Andy Edmonds <edmo@zhaw.ch>
2018-06-21 20:39:46 +0200

    minor updates

--- tests/manifests/et_manifests.py
+++ tests/manifests/et_manifests.py
@@ -17,6 +17,7 @@ services = [
 ]
 
 create_services = True
+delete_services = False  # left to inspect the services
 
 host = "http://0.0.0.0:44551"
 headers = {
@@ -53,7 +54,7 @@ def run_me():
         f.close()
 
 
-    def create_delete_svc(name, svc_id, plan_id):
+    def create_svc(name, svc_id, plan_id, delete=False):
         global url, payload, response
         url = host + "/v2/service_instances/{name}".format(name=name)
         querystring = {"accept_incomplete": "false"}
@@ -65,14 +66,15 @@ def run_me():
         }
         response = requests.request("PUT", url, data=json.dumps(payload), headers=headers, params=querystring)
         print(response.text)
-        url = host + "/v2/service_instances/{name}".format(name=name)
-        querystring = {
-            "service_id": svc_id,
-            "plan_id": plan_id,
-            "accept_incomplete": "false"
-        }
-        response = requests.request("DELETE", url, headers=headers, params=querystring)
-        print(response.text)
+        if delete:
+            url = host + "/v2/service_instances/{name}".format(name=name)
+            querystring = {
+                "service_id": svc_id,
+                "plan_id": plan_id,
+                "accept_incomplete": "false"
+            }
+            response = requests.request("DELETE", url, headers=headers, params=querystring)
+            print(response.text)
 
 
     # validate number of services registered
@@ -94,32 +96,36 @@ def run_me():
 
         # ok from here on the code gets even more ugly - for now? perhaps...
         if create_services:
-            create_delete_svc(name="test_eus_service_instance",
+            create_svc(name="test_eus_service_instance",
                               svc_id="29216b91-497c-43b7-a5c4-6613f13fa0e9",
-                              plan_id="b4cfc681-0e28-41f0-b88c-dde69169a256")
+                              plan_id="b4cfc681-0e28-41f0-b88c-dde69169a256",
+                              delete=False)
 
-            create_delete_svc(name="test_eds_service_instance",
+            create_svc(name="test_eds_service_instance",
                               svc_id="fe5e0531-b470-441f-9c69-721c2b4875f2",
-                              plan_id="94a1a0c7-21a0-42e3-abcd-f75f337b47c5")
+                              plan_id="94a1a0c7-21a0-42e3-abcd-f75f337b47c5",
+                              delete=False)
 
-            create_delete_svc(name="test_ems_service_instance",
+            create_svc(name="test_ems_service_instance",
                               svc_id="bab3ae67-8c1d-46ec-a940-94183a443825",
-                              plan_id="9b7dd476-462f-4a56-81b0-eccee8917cf7")
+                              plan_id="9b7dd476-462f-4a56-81b0-eccee8917cf7",
+                              delete=False)
 
-            create_delete_svc(name="test_ebs_service_instance",
+            create_svc(name="test_ebs_service_instance",
                               svc_id="a1920b13-7d11-4ebc-a732-f86a108ea49c",
-                              plan_id="f6ed4b3e-e132-47b6-af71-26dbb76e59cb")
+                              plan_id="f6ed4b3e-e132-47b6-af71-26dbb76e59cb",
+                              delete=False)
 
-            create_delete_svc(name="test_ess_service_instance",
+            create_svc(name="test_ess_service_instance",
                               svc_id="af7947d9-258b-4dd1-b1ca-17450db25ef7",
-                              plan_id="cfd3ebd1-5afa-420d-8313-43d681168cf7")
+                              plan_id="cfd3ebd1-5afa-420d-8313-43d681168cf7",
+                              delete=False)
 
             url = host + "/v2/et/service_instances"
 
             response = requests.request("GET", url, headers=headers)
             print(response.text)
-            if len(response.json()) > 0:
-                print("Not all instances have been deleted!")
+            print("Number of instances left running: " + str(len(response.json())))
 
 
 if __name__ == '__main__':
